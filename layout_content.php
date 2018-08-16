85da33fdb97c86afad202c26be96b60194074484
SAFIDISON <safidison.rj@gmail.com>
2018-08-16 16:33:42 +0300

    insert id login

--- layout_content.php
+++ layout_content.php
@@ -14,16 +14,37 @@
 
      echo $app->firstname;
      echo $app->id;
+      $iduser = $app->id;
 
      if (!empty($_POST['content'])) {
 
-        $user->addPerso($_POST['tuto'],$_SESSION['user_id']);
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=php_cs", 'root', 'root');
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $stmt = $conn->prepare("SELECT id FROM users WHERE id='".$iduser."'"); 
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+            $tuto = $_POST['tuto'];
+            $sql = "INSERT INTO prime(users_id,photo) VALUES ('".$iduser."','".$tuto."')";
+            // use exec() because no results are returned
+        
+            $conn->exec($sql);
+        
+            echo "Connected successfully"; 
+            }
+        catch(PDOException $e)
+            {
+            echo "Connection failed: " . $e->getMessage();
+            }
+        $conn = null;
 
     } 
 
 ?>
 
-<form action="">
+<form action="index.php" method="post">
     <input type="text" placeholder="your content here" name="tuto">
     <input type="submit" name="content">
 </form>
